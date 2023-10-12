c224deb74a0eef2a6beded87bb07487c472428fc
francois <fgg67@hotmail.fr>
2023-10-12 03:02:25 +0200

    eth() -> ether()

--- rustry_test/src/utils/constants.rs
+++ rustry_test/src/utils/constants.rs
@@ -2,6 +2,6 @@ use revm::primitives::ruint::Uint;
 
 pub const ADDRESS_ZERO: [u8; 20] = [0; 20];
 // TODO const
-pub fn eth() -> Uint<256, 4> {
+pub fn ether() -> Uint<256, 4> {
     Uint::from(10).pow(Uint::from(18))
 }
